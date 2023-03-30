6702d104401bfed1952fc69cf2949ea070b7de80
anas derk <anas.derk2020@gmail.com>
2023-03-30 03:59:16 -0700

    Design My Education Page

--- src/Components/MyEducation/index.jsx
+++ src/Components/MyEducation/index.jsx
@@ -1,8 +1,21 @@
-const MyEducation = ({ page_title }) => {
+import my_data from "../../Assets/myData/my_data.json";
+
+const MyEducation = () => {
     return (
         // Start My Education
-        <div className="my-education my-details p-3">
-            <h1>Hello { page_title }</h1>
+        <div className="my-education my-details p-4 d-grid">
+            {my_data.qualification.education.map((el, index) => (
+                /* Start Education Details Box */
+                <section className="education-details-box details-box bg-white mb-3 p-3" key={index}>
+                    <h4 className="text-center box-number mx-auto">{ index + 1 }</h4>
+                    <h6 className="duration">{el.duration}</h6>
+                    <h5 className="level">{el.level}</h5>
+                    <p className="description m-0">
+                        {el.description}
+                    </p>
+                </section>
+                /* End Education Details Box */
+            ))}
         </div>
         // End My Education
     );
