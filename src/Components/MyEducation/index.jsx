7975124dcecde3722507bdd09c18414a0bfcedae
anas derk <anas.derk2020@gmail.com>
2023-03-30 04:09:00 -0700

    Design My Experience Page

--- src/Components/MyEducation/index.jsx
+++ src/Components/MyEducation/index.jsx
@@ -3,10 +3,10 @@ import my_data from "../../Assets/myData/my_data.json";
 const MyEducation = () => {
     return (
         // Start My Education
-        <div className="my-education my-details p-4 d-grid">
+        <div className="my-education my-details p-4 d-grid align-items-center">
             {my_data.qualification.education.map((el, index) => (
                 /* Start Education Details Box */
-                <section className="education-details-box details-box bg-white mb-3 p-3" key={index}>
+                <section className="education-details-box details-box bg-white p-3" key={index}>
                     <h4 className="text-center box-number mx-auto">{ index + 1 }</h4>
                     <h6 className="duration">{el.duration}</h6>
                     <h5 className="level">{el.level}</h5>
--- src/Components/MyExperience/index.jsx
+++ src/Components/MyExperience/index.jsx
@@ -1,8 +1,21 @@
-const MyExperience = ({ page_title }) => {
+import my_data from "../../Assets/myData/my_data.json";
+
+const MyExperience = () => {
     return (
         // Start My Experience
-        <div className="my-experience my-details p-3">
-            <h1>Hello { page_title }</h1>
+        <div className="my-experience my-details p-3 d-grid align-items-center">
+            {my_data.qualification.experience.map((el, index) => (
+                /* Start Experience Details Box */
+                <section className="experience-details-box details-box bg-white mb-3 p-3" key={index}>
+                    <h4 className="text-center box-number mx-auto">{ index + 1 }</h4>
+                    <h6 className="duration">{el.duration}</h6>
+                    <h5 className="level">{el.level}</h5>
+                    <p className="description m-0">
+                        {el.description}
+                    </p>
+                </section>
+                /* End Experience Details Box */
+            ))}
         </div>
         // End My Experience
     );
