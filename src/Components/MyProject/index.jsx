6c92882e3a67dfbc6347575c714f5388e55b9daf
anas derk <anas.derk2020@gmail.com>
2023-03-30 05:19:26 -0700

    Design My Projects Page

---
+++ src/Components/MyProject/index.jsx
@@ -0,0 +1,25 @@
+import "./index.min.css";
+
+const MyProject = ({ title, description, technologies, sourceCodeLink }) => {
+    return (
+        <div className="project-box p-3">
+            <div className="name-and-description-box p-3">
+                <h5 className="project-title fw-bold">Project Name: {title}</h5>
+                <h6 className="project-description-heading mb-3 fw-bold p-2">Description :</h6>
+                <p className="project-description m-0">{description}</p>
+            </div>
+            <h6 className="project-technologies-heading fw-bold mb-3 p-2">Technologies Used :</h6>
+            <ul className="technologies-list fw-bold mb-4">
+                {technologies.map((technology, index) => (
+                    <li className="text-uppercase" key={index}>{technology}</li>
+                ))}
+            </ul>
+            <a href={sourceCodeLink} className="source-code-link btn d-block fw-bold"
+                target="_blank">
+                Code Link On Github
+            </a>
+        </div>
+    );
+}
+
+export default MyProject;
--- src/Components/MyProjects/index.jsx
+++ src/Components/MyProjects/index.jsx
@@ -1,8 +1,23 @@
-const MyProjects = ({ page_title }) => {
+import MyProject from "../../Components/MyProject/index";
+import my_data from "../../Assets/myData/my_data.json";
+
+const MyProjects = () => {
     return (
         // Start My Projects
-        <div className="my-projects my-details p-3">
-            <h1>Hello {page_title}</h1>
+        <div className="my-projects my-details p-4">
+            <div className="row">
+                {my_data.projects.map((project, index) => (
+                    <div className="col-md-6">
+                        <MyProject
+                            key={index}
+                            title={project.title}
+                            description={project.description}
+                            technologies={project.technologies}
+                            sourceCodeLink={project.sourceCodeLink}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
         // End My Projects
     );
